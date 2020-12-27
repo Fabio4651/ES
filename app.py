2d34c0562bdab287bef8118e5734d45017f30252
Sonicaru <sonicfansfh@hotmail.com>
2020-12-27 03:33:16 +0000

    logins and sessions

--- app.py
+++ app.py
@@ -1,30 +1,85 @@
 import os
-from flask import Flask, jsonify, render_template, request, make_response, redirect, url_for
-from flaskext.mysql import MySQL
+from flask import Flask, jsonify, render_template, request, session, make_response, redirect, url_for 
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
+from flask_session import Session 
+from markupsafe import escape
 import json
 import uuid
 from os.path import join, dirname, realpath
 
 #UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/img/')
 UPLOAD_FOLDER = 'static/img/'
-
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+
 """REMOVE make_response AFTER USING IT ITS NO NEEDED"""
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'test'
+#The secret key shhh 
+app.config['SECRET_KEY'] = '_1#y6G"F7Q2z\n\succ/'
 
-mysql = MySQL(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'sqlalchemy'
+
+db = SQLAlchemy(app)
+
+app.config['SESSION_SQLALCHEMY'] = db
+
+sess = Session(app)
+
+class Artigos(db.Model):
+    __tablename__ = 'artigos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(25))
+    quant = db.Column(db.Integer)
+    imgpath = db.Column(db.String(80))
+    description = db.Column(db.Text)
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20))
+    password = db.Column(db.String(20))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        data = json.loads(request.form['data'])
+
+        querydata = Users.query.filter_by(id = data['id'], password = data['pass']).first()
+
+        session['username'] = querydata.nome
+        check = {'check': 'true'}
+        return jsonify(check)
+    return 'hi'
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    session.clear()
+    check = {'check': 'true'}
+    return jsonify(check)
+
+"""
+@app.route('/set/<value>')
+def set_session(value):
+    session['value'] = value 
+    return f'The value you set is: { value }'
+
+@app.route('/get')
+def get_session():
+    return f'The value in the session is: { session.get("value") }'
+"""
 
 @app.route('/artigos')
 def artigos():
@@ -42,34 +97,9 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/artigosold", methods=["POST"])
-def artigoold_set():
-    req = request.get_json()
-    data = json.loads(json.dumps(req))
-
-    try:
-        conn = mysql.connect()
-        cur = conn.cursor()
-    except Exception:
-        print('Error: unable to connect to database')
-
-    try:
-        sql = "INSERT INTO es (nome, quant, imgpath, description) VALUES (%s, %s, %s, %s)"
-        val = (data['nome'], data['quant'], data['imgpath'], data['desc'])
-        cur.execute(sql, val)
-    except Exception:
-        print('Error: unable to insert items')
-    
-    conn.commit()
-    cur.close()
-    conn.close()
-    res = make_response(jsonify({"message": "OK"}), 200)
-    return res
-
 @app.route("/artigos", methods=["POST"])
 def artigo_set():
-    req = request.form['data']
-    data = json.loads(req)
+    data = json.loads(request.form['data'])
 
     if 'file' not in request.files:
         print('No file part')
@@ -86,17 +116,10 @@ def artigo_set():
     fullpath = UPLOAD_FOLDER+newfile+"."+filetype
 
     try:
-        conn = mysql.connect()
-        cur = conn.cursor()
+        insert = Artigos(nome=data['nome'], quant=data['quant'], imgpath=fullpath, description=data['desc'])
+        db.session.add(insert)
     except Exception:
-        print('Error: unable to connect to database')
-
-    try:
-        sql = "INSERT INTO es (nome, quant, imgpath, description) VALUES (%s, %s, %s, %s)"
-        val = (data['nome'], data['quant'], fullpath, data['desc'])
-        cur.execute(sql, val)
-    except Exception:
-        print('Error: unable to insert items')
+        print('Error: unable to insert items')    
 
     try:
         if file and allowed_file(file.filename):
@@ -105,70 +128,14 @@ def artigo_set():
     except Exception:
         print('Error: unable to save file')
 
-    conn.commit()
-    cur.close()
-    conn.close()
+    db.session.commit()
     res = make_response(jsonify({"message": "OK"}), 200)
     return res
 
 @app.route('/listartigos')
 def v_timestamp():
-    conn = mysql.connect()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM es")
-    data = cur.fetchall()
-    cur.close()
-    conn.close()
+    data = Artigos.query.all()
     return render_template('listartigos.html', data=data)
-
-@app.route("/testeroute", methods=['POST'])
-def file_test():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('No file part')
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-            flash('No selected file')
-            return redirect(request.url)
-
-        filename = file.filename
-        newfile = str(uuid.uuid4())
-        filetype = filename.rsplit('.', 1)[1].lower()
-        print(newfile+"."+filetype)
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], newfile+"."+filetype))
-        return 'hi'
-
-
-@app.route("/teste123", methods=['GET'])
-def user_get():
-    name = request.args.get('search')
-    rv = 0
-    try:
-        conn = mysql.connect()
-        cur = conn.cursor()
-    except Exception:
-        return 'Error: unable to connect to database'
-
-    try:
-        cur.execute("SELECT * FROM users WHERE id="+name)
-        row_headers=[x[0] for x in cur.description] #this will extract row headers
-        rv = cur.fetchall()
-    except Exception:
-        return 'Error: unable to fetch items'
-    finally:
-        json_data=[]
-        for result in rv:
-            json_data.append(dict(zip(row_headers,result)))
-        cur.close()
-        return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
